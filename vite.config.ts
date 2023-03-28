270762f77d90ba98889390a01d5b15d00d132527
leoff00 <leof0180@gmail.com>
2023-03-28 18:10:53 -0300

    chore: changing build dir

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: { port: 3000 },
-  build: { outDir: "./.github/workflows/build" },
+  build: { outDir: "build" },
 });
